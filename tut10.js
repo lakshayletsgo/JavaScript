638cd69a710fe90409d838c7a76535342552c626
lakshayletsgo <121108594+lakshayletsgo@users.noreply.github.com>
2023-02-09 21:43:24 +0530

    Functions

---
+++ tut10.js
@@ -0,0 +1,17 @@
+// Functions
+
+function sum(x,y){
+    return x+y;
+}
+const avg=(x,y)=>{
+    return x+y/2;
+}
+
+let a=10;
+let b=20;
+let c=30;
+
+console.log("Sum is "+sum(a,b))
+console.log("Sum is "+sum(c,b))
+console.log("Avg is "+avg(a,b))
+console.log("Avg is "+avg(c,b))
