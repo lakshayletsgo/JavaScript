c3612f995ed0ba5600d5fa5948323bcad587bc83
lakshayletsgo <121108594+lakshayletsgo@users.noreply.github.com>
2023-02-09 20:40:40 +0530

    Operators in Js

---
+++ tut6.js
@@ -0,0 +1,25 @@
+77;
+"lakshay"; //This will not throw any error
+// Operators 
+
+let a = 10;
+let b = 20;
+console.log(a+b)
+console.log(a-b)
+console.log(a/b)
+console.log(a*b)
+console.log(a**b)
+
+
+// Comparison operators are those operators like ==, <=, >= ,!= 
+//They return a boolean value
+//=== (Triple Equal to) checks the datatype of the values
+// Like
+
+let comp1 = 20;
+let comp2 ="20";
+
+console.log(comp1==comp2);
+console.log(comp1!=comp2);
+console.log(comp1===comp2);
+console.log(comp1!==comp2);
