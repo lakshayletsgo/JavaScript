dbb9d6c23294e110d2ee93961080d695d0497876
lakshayletsgo <121108594+lakshayletsgo@users.noreply.github.com>
2023-02-09 21:32:32 +0530

    While and Do-While Loops

---
+++ tut9.js
@@ -0,0 +1,16 @@
+// While Loops
+// They Work the same like the others languages
+
+let a=20;
+let i =1;
+// while(i<a){
+//     console.log(i);
+//     i++;
+// }
+
+// In Do-While loop the do function intialise the loop at least one time
+// It will return value at least one time even if it is not true
+do{
+    console.log(i);
+    i++;
+}while(i>a)
