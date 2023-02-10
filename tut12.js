861bf978b34f0706a69e4b497d648bd2c0a6f25f
lakshayletsgo <121108594+lakshayletsgo@users.noreply.github.com>
2023-02-10 16:13:37 +0530

    Strings Basics and Escape Sequences

---
+++ tut12.js
@@ -0,0 +1,20 @@
+// Strings
+let name = "Lakshay";
+console.log(name);
+console.log(name.length);
+console.log(name[2]);
+
+// Template Literal
+let name1 = "xyz"
+let name2 = "abc";
+let name3 = `${name1}  is a friend of ${name2}`
+console.log(name3)
+// String interpolation is the insertion of string directly into the template literal
+
+
+// Escape Sequence Character
+let Example = 'This is a \' example'
+console.log(Example)
+
+let example ="New\n Line"
+console.log(example)
