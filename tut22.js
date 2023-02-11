83e82f9e17699e9e4f42abd23451011bade5dd17
lakshayletsgo <121108594+lakshayletsgo@users.noreply.github.com>
2023-02-12 01:01:22 +0530

    Console Functions

---
+++ tut22.js
@@ -0,0 +1,31 @@
+// Console functions
+
+
+// This is to view all the console functions
+// console.log(console)
+
+console.assert(555>1) //This won't give anything
+console.assert(55>111) //This will give an error
+
+
+console.clear();//Isse poora console page clear ho jata hai 
+
+let obj={
+    name:"lakshay",
+    age:"19",
+    Gender:"Male",
+}
+console.table(obj) //This is used to create a table
+
+
+console.log("Hello, This is a warning") //This will give a warning, It means it will give the text in yellow colour
+
+console.error("This is  a error") //This will give the text in red colour
+
+console.info("This is information functions") //This is similar to log function, It just will be appeared in the info section.
+
+
+console.time("a");
+console.timeEnd("a");//This both functions combined will give time between them
+
+
