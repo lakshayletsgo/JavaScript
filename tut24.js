9218fd02ae2ad5e63d2bf6f5ee30c6d2bc9c6b29
lakshayletsgo <121108594+lakshayletsgo@users.noreply.github.com>
2023-02-13 23:00:53 +0530

    DOM and BOM

---
+++ tut24.js
@@ -0,0 +1,10 @@
+// DOM, BOM and Window Object
+// Window ek global object hota hai ham window.console.log bhi likh skte hai ya console.log 
+// Dono same hee hai 
+
+// Dom = Document Object Model
+// Bom = Browser Object Model
+
+
+document.body.style.background = "Yellow" //Isse hame background colour change kar skte hai 
+location.href = "www.google.com" //This will redirect to the google.com
