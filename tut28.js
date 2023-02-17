25e3b907bd1cb163c0548f3a7986fa62dbdbebb3
lakshayletsgo <121108594+lakshayletsgo@users.noreply.github.com>
2023-02-17 11:47:22 +0530

    Tables

---
+++ tut28.js
@@ -0,0 +1,15 @@
+// Tables and About them
+
+// This will happen only if the html script have table tag in it
+table.rows //This will give all the rows
+table.caption //This will give the caption element
+table.tHead//This will give head of the table
+table.tFoot//This will give Footer of the table
+table.tBodies//This will give collections of elements of tbody
+tbody.rows // Collections of tr inside
+
+tr.cells //Collections of td and th
+tr.sectionRowIndex //Index of tr inside enclosing element
+tr.rowIndex //Row number starting from 0
+
+td.cellIndex //No of cell inside enclosing tr
