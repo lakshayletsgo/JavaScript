a7b02611ec897f59235de00ab834bb8c7d93cfbb
lakshayletsgo <121108594+lakshayletsgo@users.noreply.github.com>
2023-02-17 11:35:43 +0530

    More about parents and siblings

---
+++ tut27.js
@@ -0,0 +1,10 @@
+// Learning more about parents and siblings
+
+console.log(document.body.firstChild)//This will also give comments
+console.log(document.body.firstElementChild)//This will only give element like nav,div and so on jo hame chahiye
+
+const bgcolor = ()=>{
+    document.body.style.background = "red";
+}
+
+bgcolor();//This will change the color to red
