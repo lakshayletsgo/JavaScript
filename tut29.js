4e45fa007f0464c4dca39e09839ff1cd082299e8
lakshayletsgo <121108594+lakshayletsgo@users.noreply.github.com>
2023-02-27 23:05:07 +0530

    Searching in DOM

---
+++ tut29.js
@@ -0,0 +1,18 @@
+// Searching of DOM
+// This includes functions in which we call an object with help of getElementById and getElementByClass
+// These are importants functions that are used the most 
+
+// For Example in this all the elements having class name will be selected in name1 variable 
+// let name1 = document.getElementById("name")
+
+// In this  it will select all the elements having class name1
+// let name2 = document.querySelectorAll(".name1")
+
+// This will select all the anchor tag and display them
+// console.log(document.getElementsByTagName("a"))
+
+// This will return all the anchor tags in the class card
+// console.log(document.querySelector(".card").getElementsByTagName("a"))
+
+// This will return all the tags jiske name me search hoga yeh mostly input tags me hoga
+// console.log(document.getElementsByName("search"))
