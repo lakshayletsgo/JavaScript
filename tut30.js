35fd2c775155aa0c346dd237bfeec25dad210cd5
lakshayletsgo <121108594+lakshayletsgo@users.noreply.github.com>
2023-02-27 23:16:58 +0530

    Matches, Closest and Contains

---
+++ tut30.js
@@ -0,0 +1,20 @@
+// Matches, Closest and Contains
+
+// Both will return the same output 
+// id1   But we won't use this 
+
+// let id1 = document.getElementById("id1")
+// console.log(id1)
+
+
+// IF there is a class within id1 with class class1 then it will return a true otherwise false 
+// console.log(id1.matches(".class1"))
+
+// It will return the box class if there is a class either in its parent or its parent's parent and so on
+// console.log(id1.matches(".box"))
+
+
+// let sp1 = document.getElementById("sp1")
+// It will return a true if there is a element with id sp1 otherwise false
+// console.log(id1.contains(sp1))
+// console.log(sp1.contains(sp1))       This will also return true
