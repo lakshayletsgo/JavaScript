4fe5c0d8a8d6b6eeb4bfef70cf393f3e34016a2d
lakshayletsgo <121108594+lakshayletsgo@users.noreply.github.com>
2023-03-01 17:25:39 +0530

    Browser Events

---
+++ tut37/tut37.js
@@ -0,0 +1,6 @@
+let a = document.getElementsByClassName("container")[0]
+a.onclick=()=>{
+    let b=  document.body;
+    b.innerHTML= "Helo"
+}
+//This will make the screen print helo and remove the other text
